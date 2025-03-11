fe4cd4f7c7a0fcd022d9fea148a5eee5bd03b0b6
Jonas <jonas.sousa@citiesoft.com.br>
2025-03-11 09:49:32 -0300

    feat: impl method toString to class product

--- 8-introducao-programacao-orientada-a-objetos/estoque/src/main/java/application/Main.java
+++ 8-introducao-programacao-orientada-a-objetos/estoque/src/main/java/application/Main.java
@@ -2,8 +2,12 @@ package application;
 
 import entities.Product;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
 
         String name = "TV";
         double price = 900.00;
@@ -16,11 +20,10 @@ public class Main {
 
         int addStock = 5;
         product.addProducts(addStock);
-        System.out.println("Product data: " + product.name + ", $ " + product.price + ", " + product.quantity + " units, Total: $ " + product.totalValueInStock());
+        System.out.println("Product data: " + product);
 
         int removeStock = 3;
         product.removeProducts(removeStock);
-        System.out.println("Product data: " + product.name + ", $ " + product.price + ", " + product.quantity + " units, Total: $ " + product.totalValueInStock());
-
+        System.out.println("Product data: " + product);
     }
 }
