0c4060c5d3ba48b021b70e84545e8a7c5fd42982
Jonas <jonas.sousa@citiesoft.com.br>
2025-03-11 10:54:44 -0300

    feat: second version of program circumference

--- 8-introducao-programacao-orientada-a-objetos/membros-estaticos/src/main/java/application/Main.java
+++ 8-introducao-programacao-orientada-a-objetos/membros-estaticos/src/main/java/application/Main.java
@@ -1,25 +1,18 @@
 package application;
 
-public class Main {
-    public static final double PI = 3.14159;
+import util.Calculator;
 
+public class Main {
     public static void main(String[] args) {
 
         double radius = 3.0;
 
-        double c = circumference(radius);
-        double v = volume(radius);
+        Calculator calculator = new Calculator();
+        double c = calculator.circumference(radius);
+        double v = calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", PI);
-    }
-
-    public static double circumference(double radius) {
-        return 2.0 * PI * radius;
-    }
-
-    public static double volume(double radius) {
-        return 4.0 * PI * Math.pow(radius, 3.0) / 3.0;
+        System.out.printf("PI value: %.2f%n", calculator.PI);
     }
 }
