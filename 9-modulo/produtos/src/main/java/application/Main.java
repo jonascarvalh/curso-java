7035e7f9b87f358a686d24845940134e50712810
Jonas <jonas.sousa@citiesoft.com.br>
2025-03-11 12:32:25 -0300

    feat: starting new module and intro to parameterized constructors

---
+++ 9-modulo/produtos/src/main/java/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        String name = "TV";
+        double price = 900.00;
+        int quantity = 10;
+
+        Product product = new Product(name, price, quantity);
+
+        System.out.println("Product data: " + product);
+
+        int addStock = 5;
+        product.addProducts(addStock);
+        System.out.println("Updated data: " + product);
+
+        int removeStock = 3;
+        product.removeProducts(removeStock);
+        System.out.println("Updated data: " + product);
+    }
+}
