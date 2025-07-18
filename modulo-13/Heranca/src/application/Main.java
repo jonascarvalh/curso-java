ce411a656f3c2b877f2fdc18e5d7883856a2b322
Jonas Sousa <jonas.sousa@citiesoft.com.br>
2025-07-18 13:13:03 -0300

    feat: adicionando exemplo de herança

---
+++ modulo-13/Heranca/src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Main {
+    public static void main(String[] args) {
+        // BusinessAccount has been access methods and attributes from Account
+        BusinessAccount account = new BusinessAccount();
+    }
+}
