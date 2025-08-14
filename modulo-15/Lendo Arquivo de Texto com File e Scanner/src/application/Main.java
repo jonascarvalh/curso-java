c38dece1b29ae3eb2ea2e9a7ee72aece427ac5d3
Jonas Sousa <jonas.sousa@citiesoft.com.br>
2025-08-14 15:32:52 -0300

    aula: file + scanner

---
+++ modulo-15/Lendo Arquivo de Texto com File e Scanner/src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("c:\\temp\\in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo");
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
