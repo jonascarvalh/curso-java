4c86a87ec3bd061dc43a11f3c6d21b938c480fc0
Jonas Sousa <jonas.sousa@citiesoft.com.br>
2025-08-14 14:19:12 -0300

    refact: ajustando a implementação

--- modulo-14/exercicios/bank/src/application/Main.java
+++ modulo-14/exercicios/bank/src/application/Main.java
@@ -8,34 +8,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        Integer number = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Holder: ");
+        String holder = sc.nextLine();
+
+        System.out.print("Initial Balance: ");
+        Double initialBalance = sc.nextDouble();
+
+        System.out.print("Withdraw limit: ");
+        Double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+        System.out.print("Enter amount for withdraw: ");
+        Double withdrawAmount = sc.nextDouble();
+
         try {
-            Scanner sc = new Scanner(System.in);
-
-            System.out.println("Enter account data");
-            System.out.print("Number: ");
-            Integer number = sc.nextInt();
-            sc.nextLine();
-
-            System.out.print("Holder: ");
-            String holder = sc.nextLine();
-
-            System.out.print("Initial Balance: ");
-            Double initialBalance = sc.nextDouble();
-
-            System.out.print("Withdraw limit: ");
-            Double withdrawLimit = sc.nextDouble();
-
-            Account account = new Account(number, holder, initialBalance, withdrawLimit);
-
-            System.out.print("Enter amount for withdraw: ");
-            Double withdrawAmount = sc.nextDouble();
             account.withdraw(withdrawAmount);
-
             System.out.print("New Balance: " + account.getBalance());
         } catch (BusinessException e) {
             System.out.println("Withdraw error: " + e.getMessage());
         } catch (RuntimeException e) {
             System.out.println("Unexpected error.");
         }
+
+        sc.close();
     }
 }
