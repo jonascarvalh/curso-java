718c8fb1374f7f1fd4854bc53304d10abfa63146
Jonas Sousa <jonas.sousa@citiesoft.com.br>
2025-08-14 11:51:31 -0300

    feat: reservation good example

--- modulo-14/reservation/src/model/entities/Reservation.java
+++ modulo-14/reservation/src/model/entities/Reservation.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -16,6 +18,7 @@ public class Reservation {
     }
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        validateInputs(checkIn);
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -42,21 +45,23 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut) {
         Date now = new Date();
 
         if (checkIn.before(now) || checkOut.before(now)) {
-            return "Reservation dates for update must be future dates.";
+            throw new DomainException("Reservation dates for update must be future dates.");
         }
 
-        if (!checkOut.after(checkIn)) {
-            return "Check-out date must be after check-in date.";
-        }
+        validateInputs(checkIn);
 
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+    }
 
-        return null;
+    public void validateInputs(Date checkIn) {
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date.");
+        }
     }
 
     @Override
